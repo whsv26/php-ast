082e8ad5880a0cbf698fcf98c75945555842d8bf
whsv26 <whsv26@gmail.com>
2021-05-22 23:29:44 +0300

    Separate parsers by traits.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,8 +4,9 @@ import Lexer.Codec.{given, *}
 
 @main def circe(): Unit = {
 
-//  val path = "/home/whsv26/Documents/Projects/psalmd/src/main/php/tokens.php"
+//  val path = "/home/whsv26/Documents/Projects/php-ast/src/main/php/tokens.php"
 //  println(Lexer.parseTokens(path))
 
-  println(Parser.parsed)
+  val path = "/home/whsv26/Documents/Projects/php-ast/src/main/php/parsed.php"
+  println(Parser.parse(path))
 }
--- src/main/scala/whsv26/Parser/Parser.scala
+++ src/main/scala/whsv26/Parser/Parser.scala
@@ -1,9 +1,9 @@
 package whsv26.Parser
 
+import sys.process._
 import whsv26.Lexer.Token.PhpToken
 import whsv26.Parser.Parser.{ExprAdd, Node}
 import whsv26.Parser.Parser.AstParser.{expr, rep}
-
 import scala.util.matching.Regex
 import scala.util.parsing.combinator.{Parsers, RegexParsers}
 import scala.util.parsing.input.Reader
@@ -18,38 +18,46 @@ object Parser:
   case class ExprBool(r: Boolean) extends Expr
   case class ExprInt(r: Int) extends Expr
   case class ExprFloat(r: Float) extends Expr
-  case class ExprString(r: Float) extends Expr
+  case class ExprString(r: String) extends Expr
 
+  case class ExprEqual(lhs: Expr, rhs: Expr) extends Expr
+  case class ExprEqualStrict(lhs: Expr, rhs: Expr) extends Expr
   case class ExprAdd(lhs: Expr, rhs: Expr) extends Expr
   case class ExprSub(lhs: Expr, rhs: Expr) extends Expr
   case class ExprMul(lhs: Expr, rhs: Expr) extends Expr
+  case class ExprMod(lhs: Expr, rhs: Expr) extends Expr
   case class ExprDiv(lhs: Expr, rhs: Expr) extends Expr
+  case class ExprRaw(r: String) extends Expr
 
   // Statements
   case class Ast(nodes: List[Node]) extends Stmt
   case class StmtEcho(exprs: List[Expr]) extends Stmt
-
-  val code: String = """<?php echo (1.1 - 2) + true * 5; """
+  case class StmtRaw(r: String) extends Stmt
 
   object RegCond:
     val int = "[0-9]+".r
     val float = """[0-9]+\.[0-9]*""".r
+    val string = """('|").*?('|")""".r
 
   trait ScalarParser  extends RegexParsers:
-    def scalar = bool | float | int
+    def scalar = bool | float | int | string
     def bool = ("true" | "false") ^^ { (s: String) => ExprBool(s == "true")}
     def int = RegCond.int ^^ { (s) => ExprInt(s.toInt)}
     def float = RegCond.float ^^ { (s) => ExprFloat(s.toFloat)}
+    def string = RegCond.string ^^ { (s) => ExprString(s.toString)}
 
-  trait ExpressionParser extends RegexParsers with ScalarParser:
-    def expr: Parser[Expr] = bin | scalar
-    def bin: Parser[Expr] = sum | mul
+  trait BinOpsParser extends ScalarParser:
+    def bin: Parser[Expr] = sum | mul | equal
+    def equal: Parser[Expr] = chainl1(brackets, "==" ^^^ { ExprAdd(_, _) } | "-" ^^^ { ExprSub(_, _) })
     def sum: Parser[Expr] = chainl1(mul, "+" ^^^ { ExprAdd(_, _) } | "-" ^^^ { ExprSub(_, _) })
-    def mul: Parser[Expr] = chainl1(brackets, "*" ^^^ { ExprMul(_, _) })
+    def mul: Parser[Expr] = chainl1(brackets, "*" ^^^ { ExprMul(_, _) } | "%" ^^^ { ExprMod(_, _) })
     def brackets = scalar | "(" ~> bin <~ ")"
 
+  trait ExpressionParser extends BinOpsParser:
+    def expr: Parser[Expr] = bin | scalar
+
   trait StatementParser extends ExpressionParser:
-    def stmt: Parser[Stmt] = echo
+    def stmt: Parser[Stmt] = echo | """.*\s""".r ^^ { StmtRaw(_) }
     def echo: Parser[StmtEcho] = "echo" ~> repsep(expr, ",") <~ ";" ^^ { case exprs => StmtEcho(exprs) }
 
   object AstParser extends RegexParsers with StatementParser:
@@ -57,7 +65,4 @@ object Parser:
     def codeblock: Parser[List[Stmt]] = "{" ~> rep(stmt) <~ "}"
 
 
-
-
-  val parsed = AstParser.parseAll[Ast](AstParser.ast, code)
-
+  def parse(path: String) = AstParser.parseAll[Ast](AstParser.ast, s"""cat ${path}""".!!)
