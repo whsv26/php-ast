4247744e46217c80a5a9fb9ed2b2d5efb61db851
whsv26 <whsv26@gmail.com>
2021-05-24 23:22:42 +0300

    Nodes into dedicated object.

---
+++ src/main/scala/whsv26/Parser/Node.scala
@@ -0,0 +1,38 @@
+package whsv26.Parser
+
+object Node:
+  sealed trait Node
+
+  object Statement:
+    import Expression.Expr
+    sealed trait Stmt extends Node
+    case class StmtEcho(exprs: List[Expr]) extends Stmt
+
+  object Expression:
+    sealed trait Expr extends Node
+
+    sealed trait ExprScalar extends Expr
+    case class ExprBool(r: Boolean) extends ExprScalar
+    case class ExprInt(r: Int) extends ExprScalar
+    case class ExprFloat(r: Float) extends ExprScalar
+    case class ExprString(r: String) extends ExprScalar
+
+    sealed trait ExprBinary extends Expr
+    case class ExprTernCond(cond: Expr, lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprAssign(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprAnd(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprOr(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprNotEqualStrict(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprNotEqual(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprEqualStrict(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprEqual(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprLte(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprLt(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprGte(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprGt(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprAdd(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprSub(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprMul(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprDiv(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprMod(lhs: Expr, rhs: Expr) extends ExprBinary
+    case class ExprNot(e: Expr) extends ExprBinary
--- src/main/scala/whsv26/Parser/TokensParser.scala
+++ src/main/scala/whsv26/Parser/TokensParser.scala
@@ -3,27 +3,17 @@ package whsv26.Parser
 import scala.language.implicitConversions
 import whsv26.Lexer.Token.{ComplexToken, PhpToken, SimpleToken}
 import whsv26.Parser.Parser.AstParser.{expr, rep}
-import whsv26.Parser.Parser.Expr
 import whsv26.Parser.TokensParser.opt
-
 import scala.sys.process._
 import scala.util.matching.Regex
 import scala.util.parsing.combinator.{Parsers, RegexParsers}
 import scala.util.parsing.input.{NoPosition, Position, Reader}
+import Node.Node
+import Node.Expression.*
+import Node.Statement.*
 
 object TokensParser extends TokenAwareParser:
-  sealed trait Node
-  sealed trait Stmt extends Node
-  sealed trait Expr extends Node
-
   case class Ast(nodes: List[Node])
-  case class StmtEcho(exprs: List[Expr]) extends Stmt
-
-  sealed trait ExprScalar extends Expr
-  case class ExprBool(r: Boolean) extends ExprScalar
-  case class ExprInt(r: Int) extends ExprScalar
-  case class ExprFloat(r: Float) extends ExprScalar
-  case class ExprString(r: String) extends ExprScalar
 
   def scalar = float | int | string | bool
   def int = T_LNUMBER ^^ { (t) => ExprInt(t.content.toInt)}
@@ -31,10 +21,44 @@ object TokensParser extends TokenAwareParser:
   def string = T_CONSTANT_ENCAPSED_STRING ^^ { (t) => ExprString(t.content)}
   def bool = ("true" | "false") ^^ { (s) => ExprBool(s == "true")}
 
+
+
   def ast: Parser[Ast] = T_OPEN_TAG ~> T_WHITESPACE ~> rep(stmt) <~ opt(T_CLOSE_TAG) ^^ { case stmts => Ast(stmts)}
   def stmt: Parser[Stmt] = echo
   def expr: Parser[Expr] = opt(T_WHITESPACE) ~> scalar <~ opt(T_WHITESPACE)
   def echo: Parser[StmtEcho] = T_ECHO ~> T_WHITESPACE ~> repsep(expr, T_COMMA) <~ T_SEMICOLON ^^ { case exprs => StmtEcho(exprs) }
+
+
+  def ops: Parser[Expr] = ternary | binary | unary
+  def unary: Parser[Expr] = u1
+  def u1: Parser[Expr] = "!" ~> mpr ^^ { ExprNot(_) }
+  def ternary: Parser[Expr] = t1
+  def t1: Parser[Expr] = (mpr <~ "?") ~ (mpr <~ ":") ~ mpr ^^ { case c ~ l ~ r => ExprTernCond(c, l, r) }
+  def binary: Parser[Expr] = b1
+  def b1: Parser[Expr] = chainl1(b2, "=" ^^^ ExprAssign.apply)
+  def b2: Parser[Expr] = chainl1(b3, "||" ^^^ ExprOr.apply)
+  def b3: Parser[Expr] = chainl1(b4, "&&" ^^^ ExprAnd.apply)
+  def b4: Parser[Expr] = chainl1(b5,
+    "!==" ^^^ ExprNotEqualStrict.apply |
+      "!="  ^^^ ExprNotEqual.apply |
+      "===" ^^^ ExprEqualStrict.apply |
+      "=="  ^^^ ExprEqual.apply)
+  def b5: Parser[Expr] = chainl1(b6,
+    "<="  ^^^ ExprLte.apply |
+      "<"   ^^^ ExprLt.apply |
+      ">="  ^^^ ExprGte.apply |
+      ">"   ^^^ ExprGt.apply)
+  def b6: Parser[Expr] = chainl1(b7,
+    "+"   ^^^ ExprAdd.apply |
+      "-"   ^^^ ExprSub.apply)
+  def b7: Parser[Expr] = chainl1(mpr,
+    "*"   ^^^ ExprMul.apply |
+      "/"   ^^^ ExprDiv.apply |
+      "%"   ^^^ ExprMod.apply)
+  def mpr = scalar | "(" ~> ops <~ ")"
+
+
+
 
 
   def parse(in: Input) = (ast)(in)
